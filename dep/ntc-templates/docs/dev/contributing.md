23f7df3fe2a52e95cc233b91bd1dfa2716f5f260
João Costa <joaocarlos95@gmail.com>
2024-08-08 12:03:02 +0100

    Squashed 'dep/panda/' content from commit 91a1d8c
    git-subtree-dir: dep/panda
    git-subtree-split: 91a1d8ca1ff4f5114d084aeaf9c60b129b08a9af

---
+++ dep/ntc-templates/docs/dev/contributing.md
@@ -0,0 +1,53 @@
+# Contributing
+
+Pull requests are welcomed and automatically built and tested against multiple versions of Python through GitHub Actions. 
+
+Except for unit tests, testing is only supported on Python 3.9.
+
+The project is packaged with a light development environment based on `Docker` to help with the local development of the project and to run tests within  GitHub Actions.
+
+The project is following Network to Code software development guidelines and are leveraging the following:
+
+- Black, Pylint, Bandit, Mypy, flake8, and pydocstyle for Python linting and formatting.
+- pytest, coverage, and unittest for unit tests.
+
+There are a number of things that are required in order to have a successful PR.
+
+- Docstrings must conform to the google docstring [convention](https://google.github.io/styleguide/pyguide.html#381-docstrings).
+- Following and adherence to [Extending Parsers](dev_parser.md).
+- Run tests with `invoke tests`.
+
+Documentation is built using [mkdocs](https://www.mkdocs.org/). The [Docker based development environment](dev_environment.md#docker-development-environment) can be started by running `invoke docs` [http://localhost:8001](http://localhost:8001) that auto-refreshes when you make any changes to your local files.
+
+> If creating a new vendor template, please see existing templates to look for the keys to be used. It is good to maintain a consistent data model to help in a multi-vendor environment.
+## Branching Policy
+
+The branching policy includes the following tenets:
+
+- The master branch is the primary branch to develop off of.
+- If there is a reason to have a patch version, the maintainers may use cherry-picking strategy.
+- PRs intended to add new features should be sourced from the master branch.
+- PRs intended to address bug fixes and security patches should be sourced from the master branch.
+- PRs intended to add new features that break backward compatibility should be discussed before a PR is created.
+
+NTC-Templates observes semantic versioning. This may result in a quick turn around in minor/major versions to keep pace with an ever growing feature set.
+
+## Release Policy
+
+NTC Templates currently does not have an intended release schedule cadence, and will release new features in minor versions. Any breaking changes will be done in _major_ releases. There is some development to maintain [a data model that is applicable across multiple vendors](data_model.md).
+
+When a new release is created the following should happen.
+
+- A release PR is created with:
+    - Update to the changelog in `docs/admin/release_notes/version_<major>.<minor>.md` file to reflect the changes.
+    - Change the version from `<major>.<minor>.<patch>-beta` to `<major>.<minor>.<patch>` in pyproject.toml.
+    - Set the PR to the master
+- Ensure the tests for the PR pass.
+- Merge the PR.
+- Create a new tag:
+    - The tag should be in the form of `v<major>.<minor>.<patch>`.
+    - The title should be in the form of `v<major>.<minor>.<patch>`.
+    - The description should be the changes that were added to the `version_<major>.<minor>.md` document.
+- A post release PR is created with.
+    - Change the version from `<major>.<minor>.<patch>` to `<major>.<minor>.<patch + 1>-beta` pyproject.toml.
+    - Once tests pass, merge.
---
+++ dep/ntc-templates/tests/test_index_order.py
@@ -0,0 +1,159 @@
+"""Tests to check the order of the index file."""
+
+import re
+
+from tests import load_index_data
+
+
+OS_CHOICES = [
+    "a10",
+    "alcatel_aos",
+    "alcatel_sros",
+    "allied_telesis_awplus",
+    "arista_eos",
+    "aruba_aoscx",
+    "aruba_os",
+    "avaya_ers",
+    "avaya_vsp",
+    "broadcom_icos",
+    "brocade_fastiron",
+    "brocade_netiron",
+    "brocade_nos",
+    "brocade_vdx",
+    "brocade_vyos",
+    "checkpoint_gaia",
+    "ciena_saos",
+    "cisco_apic",
+    "cisco_asa",
+    "cisco_ftd",
+    "cisco_ios",
+    "cisco_nvfis",
+    "cisco_nxos",
+    "cisco_s300",
+    "cisco_viptela",
+    "cisco_wlc",
+    "cisco_xe",
+    "cisco_xr",
+    "dell_force10",
+    "dell_powerconnect",
+    "dlink_ds",
+    "edgecore",
+    "eltex",
+    "enterasys",
+    "ericsson_ipos",
+    "extreme_exos",
+    "f5_ltm",
+    "fortinet",
+    "hp_comware",
+    "hp_procurve",
+    "huawei_smartax",
+    "huawei_vrp",
+    "ipinfusion_ocnos",
+    "juniper_junos",
+    "juniper_screenos",
+    "juniper",
+    "linux",
+    "mikrotik_routeros",
+    "oneaccess_oneos",
+    "ovs_linux",
+    "paloalto_panos",
+    "quanta_mesh",
+    "ruckus_fastiron",
+    "ubiquiti_edgerouter",
+    "ubiquiti_edgeswitch",
+    "vmware_nsxv",
+    "vyatta_vyos",
+    "vyos",
+    "watchguard_firebox",
+    "yamaha",
+    "zte_zxros",
+    "zyxel_os",
+]
+CHOICES_STRING = "|".join(OS_CHOICES)
+RE_TEMPLATE_OS = re.compile(rf"^({CHOICES_STRING})")
+
+
+def check_order(  # pylint: disable=too-many-arguments,too-many-arguments,too-many-return-statements
+    current_os,
+    prior_os,
+    cmd_len,
+    prior_len,
+    os_choices,
+    used_os,
+    cmd,
+    prior_cmd,
+):
+    """Enforcing the complex logic to ensure that the index file is ordered correctly."""
+    add_os_check = []
+
+    if current_os not in used_os and used_os is not None:
+        add_os_check.extend(used_os)
+        add_os_check.append(current_os)
+
+    if used_os != sorted(used_os):
+        msg = f"OS's are not in alpabetical order, current order: '{used_os}'"
+        return False, msg
+    if add_os_check != sorted(add_os_check):
+        msg = f"OS's are not in alpabetical order, current order: '{add_os_check}'"
+        return False, msg
+
+    if current_os not in os_choices:
+        msg = f"'{current_os}' is not one of the valid options '{used_os}'"
+        return False, msg
+
+    if not prior_os and prior_len == 0:
+        # Starting Point
+        return True, ""
+    if current_os == prior_os and cmd_len == prior_len and cmd == min(prior_cmd, cmd):
+        msg = f"OS is the same and command same length, please set {cmd} before {prior_cmd} to be in alphabetical order"
+        return False, msg
+    if current_os == prior_os and cmd_len <= prior_len:
+        # OS is the same as previous, and cmd_len is smaller or equal to prior so good
+        return True, ""
+    if current_os != prior_os and current_os not in used_os:
+        # prior OS has changed, do not need to check for length yet
+        return True, ""
+    if current_os == prior_os and cmd_len > prior_len:
+        msg = f"Current Command len '{cmd_len}' larger then previous '{prior_len}', for command '{cmd}'"
+        return False, msg
+    if current_os != prior_os and current_os in used_os:
+        msg = f"'{current_os}' OS was already used in list on command '{cmd}'"
+        return False, msg
+    msg = "Failed for unknown reason"
+    return False, msg
+
+
+def test_index_ordering():
+    prior_os = ""
+    prior_len = 0
+    prior_cmd = ""
+    used_os = []
+
+    index = load_index_data()
+    for row in index:
+        template = row[0].strip()
+        os_match = RE_TEMPLATE_OS.match(template)
+        current_os = os_match.group(0)
+        cmd_start_index = len(current_os) + 1
+        cmd = template[cmd_start_index:]
+        cmd_len = len(cmd)
+        check_val, check_msg = check_order(
+            current_os,
+            prior_os,
+            cmd_len,
+            prior_len,
+            OS_CHOICES,
+            used_os,
+            cmd,
+            prior_cmd,
+        )
+        if not check_val:
+            # assertFalse(check_val, msg=check_msg)
+            print(f"Error on line: {row}")
+            print(f"Error Message: {check_msg}")
+        assert check_val
+        if current_os not in used_os:
+            used_os.append(current_os)
+        prior_len = cmd_len
+        prior_cmd = cmd
+        prior_os = current_os
